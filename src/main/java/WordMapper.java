8114967a7957dffc9ba353001db34d37cc3dfd0c
kir_snegir <ssnegurkirill@gmail.com>
2019-11-08 12:31:46 +0300

    Scripted auto-commit on change (2019-11-08 12:31:46) by gitwatch.sh

--- src/main/java/WordMapper.java
+++ src/main/java/WordMapper.java
@@ -1,2 +1,8 @@
-public class WordMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+
 }
--- src/main/java/WordReducer.java
+++ src/main/java/WordReducer.java
@@ -1,2 +1,10 @@
-public class WordReducer {
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+
+public class WordReducer extends Reducer<Text, IntWritable, Text, LongWritable>{
+
 }
