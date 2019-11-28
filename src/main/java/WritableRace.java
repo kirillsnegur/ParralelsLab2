575890ab664fa4832e97923bd33018db767d7755
kir_snegir <ssnegurkirill@gmail.com>
2019-11-28 17:32:49 +0300

    Scripted auto-commit on change (2019-11-28 17:32:49) by gitwatch.sh

--- src/main/java/WritableRace.java
+++ src/main/java/WritableRace.java
@@ -1,10 +1,14 @@
 import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.Text;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
 public class WritableRace implements Writable {
+    public Text first;
+    public Text second;
+
     @Override
     public void readFields(DataInput dataInput) {
 
