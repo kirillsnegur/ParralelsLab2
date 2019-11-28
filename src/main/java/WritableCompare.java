b4ed5dc70ad005a92df380ef14f37831f677d36e
kir_snegir <ssnegurkirill@gmail.com>
2019-11-28 18:07:06 +0300

    Scripted auto-commit on change (2019-11-28 18:07:06) by gitwatch.sh

--- src/main/java/WritableCompare.java
+++ src/main/java/WritableCompare.java
@@ -1,6 +1,12 @@
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+
 public class WritableCompare implements WritableComparable {
+    void readFields(DataInput in);
+    void write(DataOutput out);
+    int compareTo(WritableComparable o)
 
 }
 
