a1c2f2140bdaf28fbebb7be86cc3c5fd05474c6b
kir_snegir <ssnegurkirill@gmail.com>
2019-11-29 23:45:55 +0300

    Scripted auto-commit on change (2019-11-29 23:45:55) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -2,5 +2,6 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class AirportMapper extends Mapper<LongWritable, Text, RaceComparable,  {
+public class AirportMapper extends Mapper<LongWritable, Text, RaceComparable, Text{
+    
 }
