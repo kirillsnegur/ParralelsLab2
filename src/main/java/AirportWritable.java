a5d7b8d3059253c7e1ae0983a025717e3bf2dc2a
kir_snegir <ssnegurkirill@gmail.com>
2019-11-29 18:32:01 +0300

    Scripted auto-commit on change (2019-11-29 18:32:01) by gitwatch.sh

--- src/main/java/AirportWritable.java
+++ src/main/java/AirportWritable.java
@@ -6,7 +6,7 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-public class WritableRace implements Writable {
+public class AirportWritable implements Writable {
     private Text first;
     private Text second;
 
