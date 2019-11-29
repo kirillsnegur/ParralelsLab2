14a9a01b047127a95d5b341ce5e2b1c12c9fb4f3
kir_snegir <ssnegurkirill@gmail.com>
2019-11-29 22:27:54 +0300

    Scripted auto-commit on change (2019-11-29 22:27:54) by gitwatch.sh

--- src/main/java/CSVParser.java
+++ src/main/java/CSVParser.java
@@ -3,9 +3,8 @@ import org.apache.hadoop.io.Text;
 public class CSVParser {
 
     public static String[] parseString(Text str){
-        return str.toString().split(',');
-        
+        return str.toString().split(",");
     };
 
-
+    
 }
