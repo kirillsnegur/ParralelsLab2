2657dcea0733fb81acf2734cb04a6638380c1e26
kir_snegir <ssnegurkirill@gmail.com>
2019-11-30 01:20:18 +0300

    Scripted auto-commit on change (2019-11-30 01:20:18) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -10,7 +10,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, RaceComparable, Te
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String attr[] = CSVParser.parseString(value);
         RaceComparable raceAirportId = new RaceComparable(CSVParser.getCell(attr[14]),0);
-        Text description = new Text(attr[17]);
+        Text description = new Text(attr[2]);
         context.write(raceAirportId, delay);
     }
 }
