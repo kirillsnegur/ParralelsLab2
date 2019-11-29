aaf7c31722442cf201147e8ac7fe0fbc13d3c102
kir_snegir <ssnegurkirill@gmail.com>
2019-11-29 23:46:50 +0300

    Scripted auto-commit on change (2019-11-29 23:46:50) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -2,10 +2,13 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportMapper extends Mapper<LongWritable, Text, RaceComparable, Text{
     private static int key;
 
-    protected 
-
-
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
