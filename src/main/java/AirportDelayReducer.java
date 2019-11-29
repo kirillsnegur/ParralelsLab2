d4873a20b1a325a288d04883749087ab373c0408
kir_snegir <ssnegurkirill@gmail.com>
2019-11-30 01:56:12 +0300

    Scripted auto-commit on change (2019-11-30 01:56:12) by gitwatch.sh

--- src/main/java/AirportDelayReducer.java
+++ src/main/java/AirportDelayReducer.java
@@ -1,7 +1,9 @@
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import javax.xml.soap.Text;
 
-public class AirportDelayReducer extends Reducer<RaceComparable, Text, Text, Text> {
+public class AirportDelayReducer extends Reducer<RaceComparable, Text, Text, LongWritable> {
 
+    
 }
