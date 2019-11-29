0408b082f34c497644fa50842ce6ed0b0fc14bec
kir_snegir <ssnegurkirill@gmail.com>
2019-11-30 01:57:15 +0300

    Scripted auto-commit on change (2019-11-30 01:57:15) by gitwatch.sh

--- src/main/java/AirportDelayReducer.java
+++ src/main/java/AirportDelayReducer.java
@@ -2,8 +2,12 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import javax.xml.soap.Text;
+import java.io.IOException;
 
 public class AirportDelayReducer extends Reducer<RaceComparable, Text, Text, LongWritable> {
 
-    
+    @Override
+    protected void reduce(RaceComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        
+    }
 }
