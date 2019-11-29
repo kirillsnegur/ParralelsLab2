7762d9f9cd3b4390f738515f166bb82408bafa2e
kir_snegir <ssnegurkirill@gmail.com>
2019-11-30 01:15:15 +0300

    Scripted auto-commit on change (2019-11-30 01:15:15) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -9,7 +9,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, RaceComparable, Tex
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String attr[] = CSVParser.parseString(value);
-        RaceComparable raceAirPortId = new RaceComparable();
+        RaceComparable raceAirPortId = new RaceComparable(CSVParser);
         Text delay = new Text();
         context.write(raceAirPortId, delay);
     }
