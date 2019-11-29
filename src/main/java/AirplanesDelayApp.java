b2e97e0c6931d2da15c354483325aab9cbf662ff
kir_snegir <ssnegurkirill@gmail.com>
2019-11-29 18:43:13 +0300

    Scripted auto-commit on change (2019-11-29 18:43:13) by gitwatch.sh

--- src/main/java/AirplanesDelayApp.java
+++ src/main/java/AirplanesDelayApp.java
@@ -17,8 +17,8 @@ public class AirplanesDelayApp {
         job.setJobName("AirplanesDelay");
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.setMapperClass(WordMapper.class);
-        job.setReducerClass(WordReducer.class);
+        job.setMapperClass("");
+        job.setReducerClass("");
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         job.setNumReduceTasks(2);
