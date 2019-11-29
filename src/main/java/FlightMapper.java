ff1c19710dc6cf938106b647516952d43e2a4140
kir_snegir <ssnegurkirill@gmail.com>
2019-11-30 01:00:48 +0300

    Scripted auto-commit on change (2019-11-30 01:00:48) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -1,5 +1,6 @@
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class FlightMapper extends Mapper {
-    
+public class FlightMapper extends Mapper<LongWritable,> {
+
 }
