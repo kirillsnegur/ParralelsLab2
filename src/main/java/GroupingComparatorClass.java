999188a3ee36a7ad75c875fe13c22ffea6b62680
kir_snegir <ssnegurkirill@gmail.com>
2019-12-06 21:05:12 +0300

    Scripted auto-commit on change (2019-12-06 21:05:12) by gitwatch.sh

--- src/main/java/GroupingComparatorClass.java
+++ src/main/java/GroupingComparatorClass.java
@@ -3,7 +3,7 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class GroupingComparatorClass extends WritableComparator{
 
-    
+
 
     @Override
     public int compare(Object a, Object b) {
