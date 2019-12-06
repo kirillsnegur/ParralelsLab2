9802d73a271974d8878b7c6f21a3955801e6e1e2
kir_snegir <ssnegurkirill@gmail.com>
2019-12-06 19:32:17 +0300

    Scripted auto-commit on change (2019-12-06 19:32:17) by gitwatch.sh

--- src/main/java/CSVParser.java
+++ src/main/java/CSVParser.java
@@ -10,8 +10,8 @@ public class CSVParser {
         return  (v.equals("\"ARR_DELAY\""));
     }
 
-    public static boolean isArrDelay(String v){
-        return  (v.equals("\"ARR_DELAY\""));
+    public static boolean isDesription(String v){
+        return  (v.equals(""));
     }
 
     public static int getCell(String str){
